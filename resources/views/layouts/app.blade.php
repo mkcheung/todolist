aafa8cec3edfe706cafb69d633782f99782e611e
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-07 14:16:37 -0800

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TodoList</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    <footer id="footer" class="text-center">
+        <p>Copyright &copy; 2017 TodoList</p>
+    </footer>
+    </body>
+</html>
