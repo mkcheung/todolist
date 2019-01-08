f0eebf9943195ac22c7e754ef949ee8ef36920f6
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-07 22:50:45 -0800

    add the capacity to edit todos

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
     <a class="btn btn-default" href="/">Go Back</a>
-    <h1>{{$todo->text}}</h1>
+    <h1><a href="todo/{{$todo->id}}">{{$todo->text}}</a></h1>
+    <div class="label label-danger">{{$todo->due}}</div>
+    <hr>
+    <p>{{$todo->body}}</p>
+    <br><br>
+    <a href="/todo/{{$todo->id}}/edit" class="btn btn-default">Edit</a>
 @endsection
