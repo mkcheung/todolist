4bafa4f688996d95c9f765832914e21e10f8909d
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-07 23:24:12 -0800

    add capacity to delete todo

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -103,6 +103,8 @@ class TodosController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $todo = Todo::find($id);
+        $todo->delete();
+        return redirect('/')->with('success', 'Todo Deleted');
     }
 }
